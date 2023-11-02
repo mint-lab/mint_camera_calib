53117743a4c12aec4d559372e7121650a56ef1de
ncquy <nguyencongquy.ncq@gmail.com>
2023-11-02 11:45:33 +0900

    add fisheye model

--- camera_calibration.py
+++ camera_calibration.py
@@ -127,33 +127,55 @@ class MakeTrainTestData:
         return selected_slice_train, selected_slice_test
     
 
-class CalbrationFlag:
+class CalibrationFlag:
     def __init__(self):
-        self.focal_length = {}
+        self.focal_length            = {}
+        self.focal_length['normal']  = {}
+        self.focal_length['fisheye'] = {}
 
-        self.focal_length['f']       = cv.CALIB_FIX_ASPECT_RATIO + cv.CALIB_FIX_PRINCIPAL_POINT
-        self.focal_length['fx_fy']   = cv.CALIB_FIX_PRINCIPAL_POINT
-        self.focal_length['f_cx_cy'] =  cv.CALIB_FIX_ASPECT_RATIO
+        self.focal_length['normal']['f']            = cv.CALIB_FIX_ASPECT_RATIO + cv.CALIB_FIX_PRINCIPAL_POINT
+        self.focal_length['normal']['fx_fy']        = cv.CALIB_FIX_PRINCIPAL_POINT
+        self.focal_length['normal']['f_cx_cy']      =  cv.CALIB_FIX_ASPECT_RATIO
+
+        self.focal_length['fisheye']['fx_fy']       = cv.fisheye.CALIB_FIX_SKEW + cv.fisheye.CALIB_FIX_PRINCIPAL_POINT
+        self.focal_length['fisheye']['fx_fy_s']     = cv.fisheye.CALIB_FIX_PRINCIPAL_POINT
+        self.focal_length['fisheye']['fx_fy_cx_cy'] = cv.fisheye.CALIB_FIX_SKEW
     
 
-        self.distortion = {}
+        self.distortion            = {}
+        self.distortion['normal']  = {}
+        self.distortion['fisheye'] = {}
         
-        self.distortion['k1']       = cv.CALIB_FIX_K2 + cv.CALIB_FIX_K3  + cv.CALIB_ZERO_TANGENT_DIST
-        self.distortion['k1_k2']    = cv.CALIB_FIX_K3  + cv.CALIB_ZERO_TANGENT_DIST
-        self.distortion['k1_k2_k3'] = cv. CALIB_ZERO_TANGENT_DIST
-        self.distortion['no']       = cv.CALIB_FIX_K1 + cv.CALIB_FIX_K2 + cv.CALIB_FIX_K3  + cv.CALIB_ZERO_TANGENT_DIST
+        self.distortion['normal']['k1']            = cv.CALIB_FIX_K2 + cv.CALIB_FIX_K3  + cv.CALIB_ZERO_TANGENT_DIST
+        self.distortion['normal']['k1_k2']         = cv.CALIB_FIX_K3  + cv.CALIB_ZERO_TANGENT_DIST
+        self.distortion['normal']['k1_k2_k3']      = cv. CALIB_ZERO_TANGENT_DIST
+        self.distortion['normal']['no']            = cv.CALIB_FIX_K1 + cv.CALIB_FIX_K2 + cv.CALIB_FIX_K3  + cv.CALIB_ZERO_TANGENT_DIST
         
+        self.distortion['fisheye']['k1']           = cv.fisheye.CALIB_FIX_K2 + cv.fisheye.CALIB_FIX_K3 + cv.fisheye.CALIB_FIX_K4  
+        self.distortion['fisheye']['k1_k2']        = cv.fisheye.CALIB_FIX_K3 + cv.fisheye.CALIB_FIX_K4
+        self.distortion['fisheye']['k1_k2_k3']     = cv.fisheye.CALIB_FIX_K4
+        self.distortion['fisheye']['no']           = cv.fisheye.CALIB_FIX_K1 + cv.fisheye.CALIB_FIX_K2  + cv.fisheye.CALIB_FIX_K3 + cv.fisheye.CALIB_FIX_K4
 
-    def make_cali_flag(self, focal_length, distortion):
-        if distortion == 'k1_k2_p1_p2_k3' and focal_length == 'fx_fy_cx_cy':
-            return None
-        elif focal_length == 'fx_fy_cx_cy':
-            return self.distortion[distortion]
-        elif distortion == 'k1_k2_p1_p2_k3': 
-            return self.focal_length[focal_length]
-        else:
-            return self.focal_length[focal_length] + self.distortion[distortion]
+    def make_cali_flag(self, focal_length, distortion, cam_type='normal'):
 
+        if cam_type == 'fisheye':
+            if distortion == 'k1_k2_k3_k4' and focal_length == 'fx_fy_cx_cy_s':
+                return None
+            elif focal_length == 'fx_fy_cx_cy_s':
+                return self.distortion[cam_type][distortion]
+            elif distortion == 'k1_k2_k3_k4': 
+                return self.focal_length[cam_type][focal_length]
+            else:
+                return self.focal_length[cam_type][focal_length] + self.distortion[cam_type][distortion]
+        else:   
+            if distortion == 'k1_k2_p1_p2_k3' and focal_length == 'fx_fy_cx_cy':
+                return None
+            elif focal_length == 'fx_fy_cx_cy':
+                return self.distortion[cam_type][distortion]
+            elif distortion == 'k1_k2_p1_p2_k3': 
+                return self.focal_length[cam_type][focal_length]
+            else:
+                return self.focal_length[cam_type][focal_length] + self.distortion[cam_type][distortion]
 
 def load_img(input_path):
     img_select = []
@@ -180,23 +202,28 @@ def find_chessboard_conners(images, board_pattern):
     return img_points, gray.shape
 
 
-def calibrate(train_obj_points, train_img_points, img_size, K=None, dist=None, flag_cali=None):
-    return cv.calibrateCamera(train_obj_points, train_img_points, img_size[::-1], cameraMatrix=K, distCoeffs=dist, flags=flag_cali)
+def calibrate(train_obj_points, train_img_points, img_size, cam_type='normal', K=None, dist=None, flag_cali=None):
+    if cam_type == 'fisheye':
+        return cv.fisheye.calibrate(train_obj_points, train_img_points, img_size[::-1], K=K, D=dist, flags=flag_cali)
+    else:
+        return cv.calibrateCamera(train_obj_points, train_img_points, img_size[::-1], cameraMatrix=K, distCoeffs=dist, flags=flag_cali)
 
 
-def cal_error(obj_points, img_points, rvecs, tvecs, K, dist):
+def cal_error(obj_points, img_points, rvecs, tvecs, K, dist, cam_type='normal'):
     mean_error = 0
     for i in range(len(obj_points)):
-        reproj_img_points, _ = cv.projectPoints(obj_points[i], rvecs[i], tvecs[i], K, dist)
+        if cam_type == 'fisheye':
+            reproj_img_points, _ = cv.fisheye.projectPoints(obj_points[i], rvecs[i], tvecs[i], K, dist)
+        else:
+            reproj_img_points, _ = cv.projectPoints(obj_points[i], rvecs[i], tvecs[i], K, dist)
         error = cv.norm(img_points[i], reproj_img_points, cv.NORM_L2) / len(reproj_img_points)
         mean_error += error
 
     return mean_error / len(obj_points)
 
-
 if __name__ == '__main__':
-    input_file = 'data/image/'
-    chessboard_pattern = (9, 7)
+    input_file = 'data/image_60/'
+    chessboard_pattern = (10, 7)
 
     images = load_img(input_file)
     img_points, img_size = find_chessboard_conners(images, chessboard_pattern)
@@ -204,40 +231,35 @@ if __name__ == '__main__':
     # Create object points data
     obj_3d = ObjPoints(chessboard_pattern)   
     obj_points= obj_3d.initilize_ojb_points(len(img_points))
-
-    focal_length = ['f', 'fx_fy', 'f_cx_cy', 'fx_fy_cx_cy']
-    distortion = ['k1', 'k1_k2', 'k1_k2_k3', 'k1_k2_p1_p2_k3', 'no']
-    data_split_types = [1, 2, 3, 4, 5]
-
-    frame = []
-    for data_split_type in data_split_types:
-        # Split data into train and test data
-        data = MakeTrainTestData(obj_points, img_points, chessboard_pattern)
-        obj_points_train, img_points_train, obj_points_test, img_points_test = data.split_data(split_type=data_split_type)
-
-        results = []
-        for f in focal_length:
-            result = []
-            for dist in distortion:
-                # Create a flag for calibration
-                cali_flag = CalbrationFlag()
-                flags_calibrate = cali_flag.make_cali_flag(focal_length=f, distortion=dist)
-
-                # TRAIN
-                rms_train, K_train, dist_train, rvecs_train, tvecs_train = calibrate(obj_points_train, img_points_train, 
-                                                                                    img_size, flag_cali=flags_calibrate)
-                # TEST
-                train_error = cal_error(obj_points_train, img_points_train, rvecs_train, tvecs_train, K_train, dist_train) 
-                test_error = cal_error(obj_points_test, img_points_test, rvecs_train, tvecs_train, K_train, dist_train)
-                result.append({'train': round(train_error, 3), 'test': round(test_error, 3)})
-            results.append(result)   
-        
+    print(obj_points[0])
     
-        df = pd.DataFrame(results, index= focal_length, columns=distortion)
-        frame.append(df)
+    cam_types = ['fisheye', 'normal']
+    data_split_types = [1, 2, 3, 4, 5]
+    cam_model = CameraModel()
 
-    with pd.ExcelWriter('results.xlsx') as writer:
-        df = pd.concat(frame, axis=0)
-        df.to_excel(writer, sheet_name='all')
-        for ind, df in enumerate(frame):
-            df.to_excel(writer, sheet_name= 'data_type = ' + str(ind + 1), index=False)
+    for cam_type in cam_types:
+        frame = []
+        focal_lengths = cam_model.focal_length[cam_type]
+        distortions =cam_model.distortion[cam_type]
+        for data_split_type in data_split_types:
+            # Split data into train and test data
+            data = MakeTrainTestData(obj_points, img_points, chessboard_pattern)
+            obj_points_train, img_points_train, obj_points_test, img_points_test = data.split_data(split_type=data_split_type)
+
+            results = []
+            for f in focal_lengths:
+                result = []
+                for dist in distortions:
+                    train_error, test_error = train_test_process(obj_points_train, img_points_train, obj_points_test, img_points_test, img_size, f, dist, cam_type)
+                    result.append({'train': round(train_error, 3), 'test': round(test_error, 3)})
+                results.append(result)   
+            
+        
+            df = pd.DataFrame(results, index= focal_lengths, columns=distortions)
+            frame.append(df)
+
+        with pd.ExcelWriter(cam_type + '_results_60.xlsx') as writer:
+            df = pd.concat(frame, axis=0)
+            df.to_excel(writer, sheet_name='all')
+            for ind, df in enumerate(frame):
+                df.to_excel(writer, sheet_name= 'data_type = ' + str(ind + 1), index=False)
