bc7d985d085acfa9b229c4fba9ba283a49cfe426
ncquy <nguyencongquy.ncq@gmail.com>
2023-11-27 17:25:17 +0900

    add excel file containing all information about synthetic data

--- generate_synthetic_data.py
+++ generate_synthetic_data.py
@@ -5,6 +5,7 @@ from scipy.spatial.transform import Rotation
 import random
 from camera_calibration import CameraModelCombination
 import pickle
+import pandas as pd
 
 def gen_chessboard_points(chessboard_pattern, cellsize=0.025, dtype=np.float32):
     '''Generate 3D points with regular interval on a plane (a.k.a. chessboard).'''
@@ -92,19 +93,12 @@ if __name__ == '__main__':
     chessboard_pattern = (10, 7)
     X = gen_chessboard_points(chessboard_pattern)
     
-    # K = conv_3param2K(f, image_resolution, cam_type=cam_type)
-    # print('K = ', K)
-    # rvec, tvec = conv_pose2Rt(cam_ori, cam_pos)
-    # cam_dist_coeff = generate_cam_dist_coeff(dist, cam_type=cam_type)
-    # x, _ = cv.projectPoints(X, rvec, tvec, K, cam_dist_coeff)
-    # with open('synthetic_data/img_points_synthetic.pkl', 'wb') as f:
-    #     pickle.dump(x, f)
-    
-    
-
     cam_model = CameraModelCombination()
     cam_types = ['normal']
     ind_data = 1
+    index = []
+    file_paths = []
+    camera_model = []
     for cam_type in cam_types:
         focal_lengths = cam_model.focal_length[cam_type]
         distortions = cam_model.distortion[cam_type]
@@ -119,24 +113,12 @@ if __name__ == '__main__':
                 x_noise = x + noise
                 X = np.float32(X)
                 x_noise = np.float32(np.round(x_noise, decimals = 4))
-
-                np.save('data_synthetic/synthetic_img_pt' + str(ind_data) + '.npy', x_noise) 
+                file_name = 'data_synthetic/synthetic_img_pt' + str(ind_data) + '.npy'
+                np.save(file_name, x_noise) 
                 ind_data += 1
-                
-    # Calibrate the camera using OpenCV
-    # rms, cv_K, cv_dist_coeff, cv_rvec, cv_tvec = cv.calibrateCamera([X], [x], (cam_w, cam_h), None, None, flags=cv.CALIB_ZERO_TANGENT_DIST)
+                index.append(ind_data)
+                file_paths.append(file_name)
+                camera_model.append({'type' :cam_type, 'f': f, 'dist': dist})
 
-    # print('\n### Calibrate a camera using OpenCV')
-    # with np.printoptions(precision=2, suppress=True):
-    #     print(f'* rvec: {cv_rvec[0].flatten()} (Truth: {rvec.flatten()})')
-    #     print(f'* t   : {cv_tvec[0].flatten()} (Truth: {tvec.flatten()})')
-    #     print(f'* K   : {cv_K.flatten()} (Truth: {K.flatten()})')
-    #     print(f'* dist_coeff: {cv_dist_coeff} (Truth: {cam_dist_coeff})')
-
-    # # Visualize the projection on a plane (image)
-    # f = plt.figure()
-    # plt.plot(x[:,0,0], x[:,0,1], 'k.')
-    # plt.axis('equal')
-    # plt.xlim([0, cam_w])
-    # plt.ylim([cam_h, 0])
-    # plt.show()
+    df = pd.DataFrame({'ind' :index, 'file_path': file_paths, "cam_model": camera_model})
+    df.to_excel('data_synthetic/synthetic.xlsx', index=False)
