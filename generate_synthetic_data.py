c936cd83bf71c235d039f7bb83105b99b52f1e2e
ncquy <nguyencongquy.ncq@gmail.com>
2023-12-05 17:10:53 +0900

    redefine add noise

--- generate_synthetic_data.py
+++ generate_synthetic_data.py
@@ -1,14 +1,14 @@
 import numpy as np
-import matplotlib.pyplot as plt
 import cv2 as cv
-from scipy.spatial.transform import Rotation
-import random
-from camera_calibration import CameraModelCombination
 import pandas as pd
+import random
+from scipy.spatial.transform import Rotation
+from camera_calibration import CameraModelCombination
 
-def random_principal_point():
-    cx = random.randint(image_resolution[0] / 2 - 10, image_resolution[0] / 2 + 10)
-    cy = random.randint(image_resolution[1] / 2 + 10, image_resolution[1] / 2 + 10)
+
+def random_principal_point(img_size):
+    cx = random.randint(img_size[1] / 2 + 10, img_size[1] / 2 + 10)
+    cy = random.randint(img_size[0] / 2 - 10, img_size[0] / 2 + 10)
 
     return cx, cy   
 
@@ -35,61 +35,76 @@ def generate_cam_dist_coeff(dist, cam_type='normal'):
             cam_dist_coeff[i] = round(random.uniform(random_range[0], random_range[1]), 2) 
     return cam_dist_coeff
 
-def conv_3param2K(f, image_resolution, cam_type='normal'):
+def conv_3param2K(f, img_size, cam_type='normal'):
     if cam_type == 'normal':  
         if f == 'f':
             cam_fx = cam_fy = random.randint(800, 1000)
-            cx = image_resolution[0] / 2
-            cy = image_resolution[1] / 2
+            cx = img_size[1] / 2
+            cy = img_size[0] / 2
         if f == 'fx_fy':
             cam_fx =random.randint(800, 1000)
             cam_fy = random.randint(800, 1000)
-            cx = image_resolution[0] / 2
-            cy = image_resolution[1] / 2
+            cx = img_size[1] / 2
+            cy = img_size[0] / 2
         if f == 'f_cx_cy':
             cam_fx = cam_fy = random.randint(800, 1000)
-            cx, cy = random_principal_point()
+            cx, cy = random_principal_point(img_size)
         if f == 'fx_fy_cx_cy':
             cam_fx = random.randint(800, 1000)
             cam_fy = random.randint(800, 1000)
-            cx, cy = random_principal_point()
+            cx, cy = random_principal_point(img_size)
         K = np.array([[cam_fx, 0., cx], [0., cam_fy, cy], [0., 0., 1.]])
     else: 
         if f == 'fx_fy':
             cam_fx =random.randint(800, 1000)
             cam_fy = random.randint(800, 1000)
-            cx = image_resolution[0] / 2
-            cy = image_resolution[1] / 2
+            cx = img_size[1] / 2
+            cy = img_size[0] / 2
             s = 0
         if f == 'fx_fy_s':
             cam_fx =random.randint(800, 1000)
             cam_fy = random.randint(800, 1000)
-            cx = image_resolution[0] / 2
-            cy = image_resolution[1] / 2
+            cx = img_size[1] / 2
+            cy = img_size[0] / 2
             s = round(random.uniform(0, 0.09), 2)
         if f == 'fx_fy_cx_cy':
             cam_fx =random.randint(800, 1000)
             cam_fy = random.randint(800, 1000)
-            cx, cy = random_principal_point()
+            cx, cy = random_principal_point(img_size)
             s = 0
         if f == 'fx_fy_cx_cy_s':
             cam_fx =random.randint(800, 1000)
             cam_fy = random.randint(800, 1000)
-            cx, cy = random_principal_point()
+            cx, cy = random_principal_point(img_size)
             s = round(random.uniform(0, 0.09), 2)
         K = np.array([[cam_fx, s, cx], [0., cam_fy, cy], [0., 0., 1.]])
 
     return K
-    
+
+def add_noise(x):
+    mean, standard_deviation = 0, 0.01
+    noise = np.random.normal(mean, standard_deviation, x.shape)
+    x = x + noise
+    x = np.float32(np.round(x, decimals = 4))
+
+    return x
+
+def project_points(X, rvec, tvec, K, cam_dist_coeff, cam_type='normal'):
+    if cam_type == 'fisheye':
+        x, _ = cv.fisheye.projectPoints(X, rvec, tvec, K, cam_dist_coeff)
+    else:
+        x, _ = cv.projectPoints(X, rvec, tvec, K, cam_dist_coeff)
+
+    return x
 
 if __name__ == '__main__':
     # Configure a camera
-    image_resolution = (1280, 960)
     cam_w, cam_h = (1280, 960)  # [pixel]
+    img_size = (cam_h, cam_w)
     cam_ori = [-10., -10., 0.]  # [deg] in the order of XYZ
     cam_pos = [0.1, -0.1, -0.4] # [m]
 
-    # # Project 3D points
+    # Project 3D points
     chessboard_pattern = (10, 7)
     X = gen_chessboard_points(chessboard_pattern)
     
@@ -107,18 +122,11 @@ if __name__ == '__main__':
         for f in focal_lengths:
             for dist in distortions:
                 for _ in range(10):
-                    K = conv_3param2K(f, image_resolution, cam_type=cam_type)
+                    K = conv_3param2K(f, img_size, cam_type=cam_type)
                     rvec, tvec = conv_pose2Rt(cam_ori, cam_pos)
                     cam_dist_coeff = generate_cam_dist_coeff(dist, cam_type=cam_type)
-                    if cam_type == 'fisheye':
-                        x, _ = cv.fisheye.projectPoints(X, rvec, tvec, K, cam_dist_coeff)
-                    else:
-                        x, _ = cv.projectPoints(X, rvec, tvec, K, cam_dist_coeff)
-                    mean, standard_deviation = 0, 0.01
-                    noise = np.random.normal(mean, standard_deviation, x.shape)
-                    x = x + noise
-                    X = np.float32(X)
-                    x = np.float32(np.round(x, decimals = 4))
+                    x = project_points(X, rvec, tvec, K, cam_dist_coeff, cam_type=cam_type)
+                    x = add_noise(x)
                     file_name = 'data_synthetic/synthetic_img_pt' + str(ind_data) + '.npy'
                     np.save(file_name, x) 
                     index.append(ind_data)
