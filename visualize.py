0b12fa0c20208e7e9d26db6c91646a1445816f58
ncquy <nguyencongquy.ncq@gmail.com>
2024-07-01 17:22:35 +0900

    add point wise rmse visualization

--- visualize.py
+++ visualize.py
@@ -1,48 +1,13 @@
 import os
+import argparse
 import numpy as np
-import seaborn as sn
 import pandas as pd
+import json
 import matplotlib.pyplot as plt
+import seaborn as sns
+import cv2 as cv
+from camera_calibration import *
 
-def caculate_model_score(train_error, test_error):
-    test_error_weight = 1
-    train_error_weight = 1
-    ratio_weight = 1
-
-    train_test_ratio = train_error / test_error
-
-    # return -np.log(test_error_weight / test_error + train_error_weight / train_error + ratio_weight * train_test_ratio)
-    num_para = get_num_para()
-    return np.log(train_error + test_error + pow(train_error, 2) / test_error + num_para * train_error)
-
-def get_num_para():
-    dists = [0, 1, 2, 3, 5, 0, 1, 2, 3, 4]
-    intrinsics = [1, 2, 3, 4]
-
-    num_para = []
-    for intrinsic in intrinsics:
-        tmp = []
-        for dist in dists:
-            tmp.append(dist + intrinsic)
-        num_para.append(tmp)
-
-    return np.array(num_para)
-
-def cal_AIC(RMSE, N_samples):
-    proj_model_num_para = [1, 2, 3, 4]
-    dist_model_num_para = [0, 1, 2, 3, 5, 0, 1, 2, 3, 4]
-
-    num_para = np.array([[dist + f for dist in dist_model_num_para] for f in proj_model_num_para])
-
-    return N_samples * np.log(pow(RMSE, 2)) + 2 * num_para
-
-def cal_BIC(RMSE, N_samples):
-    proj_model_num_para = [1, 2, 3, 4]
-    dist_model_num_para = [0, 1, 2, 3, 5, 0, 1, 2, 3, 4]
-
-    num_para = np.array([[dist + f for dist in dist_model_num_para] for f in proj_model_num_para])
-
-    return N_samples * np.log(pow(RMSE, 2)) + num_para * np.log10(N_samples)
 
 def normalize(array):
     min_val = np.min(array)
@@ -51,93 +16,69 @@ def normalize(array):
 
     return array
 
-def load_score(file_path):
-    AIC_score = np.load(file_path + 'AIC/AIC.npy')
-    AIC_score = normalize(AIC_score)
-
-    BIC_score = np.load(file_path + 'AIC/BIC.npy')
-    BIC_score = normalize(BIC_score)
-
-    train_error = np.load(file_path + 'proposal/train.npy')
-    test_error = np.load(file_path + 'proposal/test.npy')
-    proposal_score = caculate_model_score(train_error, test_error)
-    proposal_score = normalize(proposal_score)
-
-    return AIC_score, BIC_score, proposal_score
-
-def normalize(array):
-    min_val = np.min(array)
-    max_val = np.max(array)
-    array = (array - min_val) / (max_val - min_val)
-
-    return array
-
-def plot_one_score(df):
-    
+def visualize_model_wise(df):
     # plt.figure(figsize=(10,7))
-    sn.set(font_scale=0.65) # for label size
-    sn.heatmap(df, annot=True, cmap='jet', square=True) # font size
-    plt.plot([5, 5], [0, 4], 'k-', linewidth=3)
+    sns.set(font_scale=0.65) # for label size
+    sns.heatmap(df, annot=True, cmap='jet', square=True) # font size
     # plt.axis('equal')
     plt.show()
 
-def plot_multi_score(df_AIC, df_BIC, df_proposal):
-    fig, axes = plt.subplots(3, 1, figsize=(10, 7))
-    sn.set(font_scale=0.7)
-
-    # Plot the first heatmap
-    AIC = sn.heatmap(df_AIC, ax=axes[0], annot=True, cmap='jet', square=True)
-    AIC.set_xticklabels(AIC.get_xticklabels(), fontsize=8)
-    AIC.set_yticklabels(AIC.get_yticklabels(), fontsize=8)
-    axes[0].plot([5, 5], [0, 4], 'k-', linewidth=2.5)
-    axes[0].set_title('AIC', fontsize=12)
-
-    # Plot the second heatmap
-    BIC = sn.heatmap(df_BIC, ax=axes[1],  annot=True, cmap='jet', square=True)
-    BIC.set_xticklabels(BIC.get_xticklabels(), fontsize=8)
-    BIC.set_yticklabels(BIC.get_yticklabels(), fontsize=8)
-    axes[1].plot([5, 5], [0, 4], 'k-', linewidth=2.5)
-    axes[1].set_title('BIC', fontsize=12)
-
-    # Plot the third heatmap
-    proposal = sn.heatmap(df_proposal, ax=axes[2],  annot=True, cmap='jet', square=True)
-    proposal.set_xticklabels(proposal.get_xticklabels(), fontsize=8)
-    proposal.set_yticklabels(proposal.get_yticklabels(), fontsize=8)
-    axes[2].plot([5, 5], [0, 4], 'k-', linewidth=2.5)
-    axes[2].set_title('Proposal', fontsize=12)
-
+def visualize_point_wise(error, img_name):
+    plt.imshow(error, cmap='jet', interpolation='nearest', aspect='auto')
+    plt.colorbar()  
+    plt.title('Point wise error')  
+    plt.xlabel('Image') 
+    plt.xticks(ticks=np.arange(0, len(img_name)), labels=img_name, rotation='vertical')
+    plt.ylabel('Point')
     plt.tight_layout()
     plt.show()
 
+def normalize_df(df):
+    return (df - df.stack().min()) / (df.stack().max() - df.stack().min())
+
 
 if __name__ == '__main__':
-    file_path = 'data/synthetic/dataset_noise_1/model_23' 
-    index = ['P0', 'P1', 'P2', 'P3']
-    dist_model_BC = ['BC0', 'BC1', 'BC2', 'BC3']
-    proj_model_KB = ['P1', 'P3']
-    dist_model_KB =['KB0', 'KB1', 'KB2']
-    N_samples = 40 * 70
+    # Add arguments thi the parser
+    parser = argparse.ArgumentParser(prog='visualize', description='Visualization of model wise score or point wise rmse')
+    parser.add_argument('result_file', type=str, help='specify the result file path')
+    parser.add_argument('-t', '--type_visualization', default='model_wise_socre', type=str, help='specify if user want to display model wise score or point wise rmse')
+    parser.add_argument('-p', '--chessboard_pattern', default=(10, 7), type=tuple, help='Specify the chessboard pattern size')
 
-    # Load error
-    rms = np.load(os.path.join(file_path, 'rms.npy'))
-    train_error = np.load(os.path.join(file_path, 'train_error_extrapolar.npy'))
-    test_error = np.load(os.path.join(file_path, 'test_error_extrapolar.npy'))
+    # Parse the command-line arguments
+    args = parser.parse_args()
 
-    # AIC 
-    AIC = cal_AIC(rms, N_samples)
-    AIC = normalize(AIC)
-    df_AIC = pd.DataFrame(AIC, index=index, columns=column)
-    # plot_one_score(df_AIC)
+    file_path = args.result_file
+    with open(file_path, 'r') as json_file:
+        results = json.load(json_file)
 
-    # BIC
-    BIC = cal_BIC(rms, N_samples)
-    BIC = normalize(BIC)
-    df_BIC = pd.DataFrame(BIC, index=index, columns=column)
+    if args.type_visualization == 'model_wise_socre':
+        model_wise_df = results['model_wise_score']
+        model_wise_df = pd.DataFrame.from_dict(model_wise_df, orient='index')
+        visualize_model_wise(normalize_df(model_wise_df))
+    elif args.type_visualization == 'point_wise_rmse':
+        pattern = args.chessboard_pattern
+        imgs, img_name = load_img(results['data_path'])
+        img_pts, img_size = find_chessboard_corners(imgs, pattern)
 
-    # Proposal
-    proposal_core = caculate_model_score(train_error, test_error)
-    proposal_core = normalize(proposal_core)
-    df_proposal = pd.DataFrame(proposal_core, index=index, columns=column)
+        obj_pts = generate_obj_points(pattern, len(img_pts))
+        N_samples = len(obj_pts) * obj_pts[0].shape[0]
+        cam_w, cam_h = img_size[1], img_size[0]
 
-    # Plot AIC, BIC, proposal
-    plot_multi_score(df_AIC, df_BIC, df_proposal)
+        dist_type = results['best_dist_model']
+        intrinsic_type1 = results['best_proj_model']
+        calibrate_flag = CalibrationFlag()
+        flags = calibrate_flag.make_flag(intrinsic_type1, dist_type)
+        rms, K, dist_coef, rvecs, tvecs = calibrate(obj_pts, img_pts, img_size, dist_type=dist_type, flags=flags)
+        error = []
+        for i in range(len(obj_pts)):
+            reproj_img_pts = find_reproject_points(obj_pts[i], rvecs[i], tvecs[i], K, dist_coef, dist_type)
+            diff = reproj_img_pts - img_pts[i]
+            error.append(np.array([cv.norm(d, cv.NORM_L2) for d in diff]))
+        
+        error = np.array(error).T
+        pt = [f'pt{i}' for i in range(1, pattern[0] * pattern[1] + 1)]
+        point_wise_rmse = pd.DataFrame(error, columns=img_name, index=pt)
+        file_name = [file.split('.')[0] for file in img_name]
+        visualize_point_wise(error, file_name)
+        
+        
